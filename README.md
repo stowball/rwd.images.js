c06ac6992571879ea59678081c95c6ceb2da20b1
Matt Stow <matt@mattstow.com>
2014-02-04 23:27:44 +1100

    Initial commit

---
+++ README.md
@@ -0,0 +1,111 @@
+# rwd.images.js
+
+## Yet another lightweight, customisable responsive image solution
+
+I know what you're thinking. Aren't there enough responsive image solutions by now? Until `<picture>` is standard and we can use it everywhere, I'm going to say "no".
+
+### Why is this one any different?
+
+I like to think that it's a best-of-all-worlds solution, which addresses the requirements that Mat Marquis lists in his article [So, You're Writing A Responsive Images Script](http://filamentgroup.com/lab/respimg_scripts/):
+
+* Only makes 1 request for the image's current situation.
+* Easily allows a `<noscript>` fallback for users without JavaScript. If JavaScript breaks then so does the script, but that goes for all non-native solutions.
+* It supports "art direction", ie. the ability to load "sources that make use of different cropping, zooming, and focal points".
+* It actually works in the real-world. I've been using early prototypes of the plugin internally at [XO Digital](http://xodigital.com.au), and have just included this in our latest soon-to-be-released project for a well-recognised brand.
+
+In addition to the above, rwd.images.js:
+
+* Uses a familar CSS/media query syntax for determining the appropriate image src.
+* Can automatically convert pixel-based media queries to ems.
+* Supports high-DPI/retina versions when a consistent naming convention is used.
+* Supports fallback images for browsers that don't support media queries.
+* Supports lazy-loading of images.
+* Is not restricted to just `<img>` tags. It can be applied to any element (`<div>`s etc) for use as backgrounds.
+* Works using either a mobile-first or a desktop-down philosophy.
+* Has no dependencies, but can piggy-back off [enquire.js](http://wicky.nillia.ms/enquire.js/) to allow users to "right-click" and share.
+* Is only ~1.7 KB minified and gzipped.
+
+### OK. Show me how it works
+
+rwd.images.js uses the `.rwdimage` class, a series of `[data-rwdimage]` attributes and "CSS" to set up a responsive image, like so:
+
+```html
+<img class="rwdimage" data-rwdimage="
+	{ src: url(/images/4-by-3.jpg); padding-bottom: ratio(3/4); },
+	(min-width: 501px) { src: url(/images/16-by-9.jpg); padding-bottom: ratio(9/16); }
+" />
+
+<script src="/js/rwd.images.min.js"></script>
+</body>
+```
+
+#### So what's this doing?
+
+1. `.rwdimage` instructs the script that this image needs to be "made responsive".
+2. `[data-rwdimage]` is passed a comma-separated list of CSS-like rules without a selector:
+	1. `src` is the path to the image that should be set for that breakpoint (or by default if no media query is used)
+	2. `padding-bottom: ratio(y,x)` gets evaluated and converted in to an [intrinsic percentage ratio](http://alistapart.com/article/creating-intrinsic-ratios-for-video/). You can pass in the actual dimensions of the image, or break it down to its smallest fraction like I have here.
+
+So, the `<img>`:
+
+* Will display `4-by-3.jpg` by default; and
+* At 501px, will be switched to display `16-by-9.jpg`.
+
+### Taking it further
+
+Now let's use some more of the options available in rwd.images.js:
+
+```html
+<img class="rwdimage" data-rwdimage="
+	{ src: url(/images/4-by-3.jpg); padding-bottom: ratio(3/4); },
+	(min-width: 501px) { src: url(/images/16-by-9.jpg); padding-bottom: ratio(9/16); }
+" data-rwdimage-fallback="
+	{ src: url(/images/16-by-9.jpg); padding-bottom: ratio(9/16); }
+" data-rwdimage-em="true" data-rwdimage-retina="true" />
+```
+
+The `<img>`:
+
+* Will display `4-by-3.jpg` by default
+	* But `4-by-3@2x.jpg` for high-DPI/retina devices
+* At 31.3125em (501 ÷ 16) will display `16-by-9.jpg`
+	* But `16-by-9@2x.jpg` for high-DPI devices
+* Will also display `16-by-9.jpg` for the oldIE fallback (`.ltie9`)
+
+So here we can see how em-based media queries, retina images and old IE can easily be supported using rwd.images.js with minimal effort.
+
+### Further still
+
+Using the remaining `[data-rwdimage-*]` attributes, we can achieve even more:
+
+* `[data-rwdimage-em-base]`: To change the base font size for the calculation of media queries from 16px. Some developers like to set `html { font-size: 62.5%; }` so set `-em-base` to 10.
+* `[data-rwdimage-fallback-class]`: The class (generally on `html`) used to target the fallback browser. The default is `ltie9`, but you may like to set it to `lt-ie9` or `no-mq` if using Modernizr to detect media query support.
+* `[data-rwdimage-retina-suffix]`: The suffix used for high-DPI/retina image filenames. The default is `@2x`. Filenames must follow the convention: [filename][suffix][extension].
+* `[data-rwdimage-lazy-load]`: Set to `true`to only load the `<img>`'s relevant src when it has a class of `lazy-loaded` applied.
+
+Since rwd.images.js solves the responsive image problem with a `background-image` "workaround", by loading [@WickyNilliams'](https://twitter.com/WickyNilliams) [enquire.js](http://wicky.nillia.ms/enquire.js/) before rwd.images.js, the `<img src>` will also be set to the correct filename so users can share or download it.
+
+Note: If using enquire.js and `[data-rwdimage-lazy-load="true"]`to lazy-load images, you must also call `window.rwdImageChangeSrc(img)` when applying the `.lazy-loaded` class.
+
+Something like:
+
+```html
+<script src="/js/enquire.min.js"></script>
+<script src="/js/rwd.images.min.js"></script>
+<script>
+(function() {
+	// Get all images
+	var $images = document.getElementsByClassName('rwdimage');
+	
+	var lazyLoad = function($image) {
+		$image.className += ' lazy-loaded';
+		window.rwdImageChangeSrc($image);
+	};
+	
+	// Lazy-load them all (don't use this in production!)
+	for (var i = 0; i < $images.length; i++) {
+		lazyLoad($images[i]);
+	}
+})();
+</script>
+```
---
+++ rwd.images.js
@@ -0,0 +1,190 @@
+/*!
+ * RWD Images v0.2.0
+ *
+ * A lightweight, customisable responsive image solution, which uses a familar media query syntax
+ *
+ * Copyright (c) 2014 Matt Stow
+ *
+ * http://mattstow.com
+ *
+ * Licensed under the MIT license
+*/
+;(function(document) {
+	var $rwdImages = document.getElementsByClassName('rwdimage')
+		rwdImagesLength = $rwdImages.length;
+	
+	if (rwdImagesLength === 0)
+		return;
+	
+	var $this,
+		selector,
+		dataCore,
+		dataCoreLength,
+		dataCoreCurrent,
+		dataLazyLoad,
+		dataRetina,
+		dataRetinaSuffix,
+		dataEm,
+		dataEmBase,
+		dataFallback,
+		dataFallbackClass,
+		css = '',
+		cssTemp,
+		width,
+		height,
+		ratio,
+		images = [],
+		html,
+		i = 0,
+		x = 0,
+		hasQuerySelector = !!document.querySelector,
+		hasEnquire = !!window.enquire,
+		styleTag;
+	
+	for (i; i < rwdImagesLength; i++) {
+		$this = $rwdImages[i];
+		
+		// Give it a data-rwdimage-id to be used as a CSS selector
+		$this.setAttribute('data-rwdimage-id', i);
+		selector = '[data-rwdimage-id="' + i + '"] ';
+		
+		// Get all of the "core" CSS rules applied
+		dataCore = $this.getAttribute('data-rwdimage').split(',');
+		dataCoreLength = dataCore.length;
+		
+		images[i] = {};
+		images[i]['breakpoints'] = new Array(dataCoreLength);
+		
+		// If the elem is an image
+		if ($this.tagName.toLowerCase() === 'img') {
+			// and it doesn't have a src, set it to as base64 blank gif for completeness
+			if (!$this.src)
+				$this.src = 'data:image/gif;base64,R0lGODlhAQABAIAAAP///wAAACH5BAEHAAAALAAAAAABAAEAAAICRAEAOw==';
+			
+			images[i]['isImage'] = true;
+		}
+		
+		if (hasQuerySelector)
+			images[i]['elem'] = document.querySelector(selector);
+		
+		dataEm = $this.getAttribute('data-rwdimage-em') === 'true' ? true : false;
+		dataEmBase = $this.getAttribute('data-rwdimage-em-base') ? parseInt($this.getAttribute('data-rwdimage-em-base')) : 16;
+		
+		cssTemp = '';
+		
+		var cssReplace = function(str) {
+			// Remove any leading whitespace, change image: to background-image and calculate ratio percentages
+			return str
+					.replace(/(^\s*)/, '')
+					.replace(/src:\s*/gi, 'background-image: ')
+					.replace(/ratio\((\d+\/\d+)\)/gi, function(str, p1) {
+						return (eval(p1) * 100) + '%';
+					});
+		};
+		
+		for (var j = 0; j < dataCoreLength; j++) {
+			dataCoreCurrent = cssReplace(dataCore[j]);
+			
+			images[i]['breakpoints'][j] = {};
+			
+			// If it contains a min or max media query...
+			if (dataCoreCurrent.match(/^\(\s*?(min|max)/)) {
+				// Create a full media query selector
+				
+				// If specified, convert pixel media queries to ems
+				if (dataEm) {
+					dataCoreCurrent = dataCoreCurrent.replace(/m(?:in|ax)-(?:width|height):\s*(\d+)px/gi, function(str, p1) {
+						return str.replace(p1, parseInt(p1, 10) / dataEmBase).replace('px', 'em')
+					});
+				}
+				
+				// Write the new, complete selector
+				cssTemp += '@media ' +
+						dataCoreCurrent
+						.match(/.*?\{/)[0] +
+						selector +
+						dataCoreCurrent.replace(/(\((min|max).*?\)|\s?(and)\s?)/g, '') +
+						' }\n';
+				
+				// Store all of the breakpoints in an array
+				images[i]['breakpoints'][j]['mediaquery'] = dataCoreCurrent.match(/(.*?)(\{)/)[1];
+			}
+			// Otherwise output a plain selector
+			else {
+				cssTemp += selector + dataCoreCurrent + '\n';
+				images[i]['breakpoints'][j]['mediaquery'] = '';
+			}
+		}
+		
+		dataRetina = $this.getAttribute('data-rwdimage-retina') === 'true' ? true : false;
+		dataRetinaSuffix = $this.getAttribute('data-rwdimage-retina-suffix') ? $this.getAttribute('data-rwdimage-retina-suffix') : '@2x';
+		
+		// If we're duplicating for "retina"
+		if (dataRetina) {
+			cssTemp = cssTemp
+					// Find any media queries, duplicate and append the relevant high DPI media query to it
+					.replace(/(^@media\s*?)(\(.*?)\{(.*?\[.*?)(\..*?\))(.*)/gm, '$1$2$3$4$5\n$1$2 and (min--moz-device-pixel-ratio: 1.3), $2 and (-o-min-device-pixel-ratio: 2.6/2), $2 and (-webkit-min-device-pixel-ratio: 1.3), $2 and (min-device-pixel-ratio: 1.3), $2 and (min-resolution: 1.3dppx) \{$3' + dataRetinaSuffix + '$4$5')
+					// If it doesn't have an existing media query, wrap it in a high DPI one
+					.replace(/(^\[.*?)(\..*?\))(.*)/gm, '$1$2$3\n@media (min--moz-device-pixel-ratio: 1.3), (-o-min-device-pixel-ratio: 2.6/2), (-webkit-min-device-pixel-ratio: 1.3), (min-device-pixel-ratio: 1.3), (min-resolution: 1.3dppx) { $1' + dataRetinaSuffix + '$2$3 }');
+		}
+		
+		dataFallback = $this.getAttribute('data-rwdimage-fallback');
+		
+		// If we're having a fallback for .ltie9 or .no-mq
+		if (dataFallback) {
+			dataFallbackClass = $this.getAttribute('data-rwdimage-fallback-class') ? '.' + $this.getAttribute('data-rwdimage-fallback-class') : '.ltie9';
+			
+			// Output the relevant selector
+			cssTemp +=  dataFallbackClass +
+						' ' +
+						selector +
+						cssReplace(dataFallback) +
+						'\n';
+		}
+		
+		// If we're lazy-loading the images, add .lazy-loaded to the selector
+		if ($this.getAttribute('data-rwdimage-lazy-load') === 'true')
+			cssTemp = cssTemp.replace(/(\[data-rwdimage-id="\d+"\])/g, '.lazy-loaded$1');
+		 
+		css += cssTemp;
+	}
+	
+	// Output the default styles and all of the generated rules
+	css = '.rwdimage { background-repeat: no-repeat; background-size: contain; height: 0; width: 100%; }\n' + css;
+	
+	styleTag = document.createElement('style');
+	styleTag.appendChild(document.createTextNode(css));
+	document.head.appendChild(styleTag);
+	
+	window.hasComputedStyle = !!window.getComputedStyle;
+	
+	// Change the img src to its computed background-image src. If lazy-loading, fire this externally as DOMAttrModified is too aggressive
+	window.rwdImageChangeSrc = function(image) {
+		if (hasComputedStyle && image.tagName.toLowerCase() === 'img') {
+			var newsrc = window.getComputedStyle(image).getPropertyValue('background-image').replace(/url\((?:"|')?(.*?)(?:"|')?\)/, '$1');
+			if (newsrc !== 'none' && image.src !== newsrc)
+				image.src = newsrc;
+		}
+	};
+	
+	// Register with enquire.js on each of the images' media queries to change their src
+	var registerWithEnquire = function(x, y) {
+		enquire.register(images[x]['breakpoints'][y]['mediaquery'], function () {
+			rwdImageChangeSrc(images[x]['elem']);
+		});
+	};
+	
+	// Loop through any images and set their src using the 2 functions above.
+	for (x; x < images.length; x++) {
+		if (images[x]['isImage']) {
+			for (var y = 0; y < images[x]['breakpoints'].length; y++) {
+				if (hasQuerySelector && hasComputedStyle) {
+					if (hasEnquire)
+						registerWithEnquire(x, y);
+					else
+						rwdImageChangeSrc(images[x]['elem']);
+				}
+			}
+		}
+	}
+})(document);
